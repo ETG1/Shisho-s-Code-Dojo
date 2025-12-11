7a7428a5765568cb079d8097f0845224481b4446
ETG1 <elijahkazungu22@gmail.com>
2025-12-11 23:50:18 +0200

    Hero

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -7,14 +7,13 @@ function Hero() {
     <div className='w-full relative h-screen overflow-hidden'>
       <Image src={'/hero.gif'} alt='hero' width={1000} height={1000} className='w-full h-full object-cover absolute inset-0' 
       />
-      <div className='absolute w-full flex flex-col items-center mt-38'>
+      <div className='absolute w-full flex flex-col items-center mt-48'>
         <h2 className=' text-7xl font-game text-center'>Start Your</h2>
         <h2 className='font-bold text-8xl font-game text-center text-gray-400  rounded-lg '
         style={{
           textShadow:"2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000,-2px 2px 0 #000"
         }}
         >Coding Journey </h2>
-        <h2 className='font-bold text-5xl font-game text-center'>With Shisho</h2>
         <p className='mt-5 font-game text-3xl'>
           Begginer friendly coding courses and projects
         </p>
